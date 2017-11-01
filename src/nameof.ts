9be3c62d08feb148f02b84ca16837c3fc693c97d
Who Cares <it@doesnt.matter>
2017-11-01 10:20:34 -0500

    Fixed tsconfig for es5 targets and nameof for minified code.

--- src/nameof.ts
+++ src/nameof.ts
@@ -6,18 +6,40 @@ export function nameof<T extends Object>(selector: (obj: T) => any): string {
     let fnStr: string = selector.toString();
     let dotIndex: number = fnStr.indexOf(".");
     if (dotIndex > -1) {
+        // ES5
         // "function(x) { return x.prop; }"
         // or
-        // "(x) => { return x.prop; }"
+        // "function(x) { return x.prop }"
+        // or
+        // "function(x) {return x.prop}"
         if (fnStr.indexOf("{") > -1) {
-            return fnStr.substring(dotIndex + 1, fnStr.lastIndexOf(";"));
+            let endingIndex = -1;
+            let endsWithSemicolon = fnStr.lastIndexOf(";");
+            let endsWithSpace = fnStr.lastIndexOf(" }");
+            let endsWithBrace = fnStr.lastIndexOf("}");
+
+            if (endsWithSemicolon > -1) {
+                endingIndex = endsWithSemicolon;
+            }
+            else if (endsWithSpace > -1) {
+                endingIndex = endsWithSpace;
+            }
+            else if (endsWithBrace > -1) {
+                endingIndex = endsWithBrace;
+            }
+            else {
+                throw new Error("ts-simple-nameof: Invalid function syntax.");
+            }
+
+            return fnStr.substring(dotIndex + 1, endingIndex);
         }
+        // ES6
         // "(x) => x.prop"
         else {
             return fnStr.substr(dotIndex + 1);
         }
     }
     else {
-        throw new Error("Invalid property accessor function.");
+        throw new Error("ts-simple-nameof: Invalid property accessor function.");
     }
 }
